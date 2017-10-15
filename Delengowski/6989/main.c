1d74725f2885d81db7a444e16ceedc5d71555205
Delengowski <delengowm1@students.rowan.edu>
2017-10-15 17:01:27 -0400

    updated settings again

--- Delengowski/6989/main.c
+++ Delengowski/6989/main.c
@@ -5,8 +5,8 @@ int main(void)
   WDTCTL = WDTPW | WDTHOLD;                 // Stop Watchdog
 
   // Configure GPIO
-  P2SEL0 |= BIT0 | BIT1;                    // USCI_A0 UART operation
-  P2SEL1 &= ~(BIT0 | BIT1);
+  P3SEL0 |= BIT4 | BIT5;                    // USCI_A1 UART operation
+  P3SEL1 &= ~(BIT4 | BIT5);
 
   // Disable the GPIO power-on default high-impedance mode to activate
   // previously configured port settings
@@ -19,28 +19,28 @@ int main(void)
   CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
   CSCTL0_H = 0;                             // Lock CS registers
 
-  // Configure USCI_A0 for UART mode
-  UCA0CTLW0 = UCSWRST;                      // Put eUSCI in reset
-  UCA0CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
+  // Configure USCI_A1 for UART mode
+  UCA1CTLW0 = UCSWRST;                      // Put eUSCI in reset
+  UCA1CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
   // Baud Rate calculation
   // 8000000/(16*9600) = 52.083
   // Fractional portion = 0.083
   // User's Guide Table 21-4: UCBRSx = 0x04
   // UCBRFx = int ( (52.083-52)*16) = 1
-  UCA0BR0 = 52;                             // 8000000/16/9600
-  UCA0BR1 = 0x00;
-  UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
-  UCA0CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
-  UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+  UCA1BR0 = 52;                             // 8000000/16/9600
+  UCA1BR1 = 0x00;
+  UCA1MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+  UCA1CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
+  UCA1IE |= UCRXIE;                         // Enable USCI_A1 RX interrupt
 
   __bis_SR_register(LPM3_bits | GIE);       // Enter LPM3, interrupts enabled
   __no_operation();                         // For debugger
 }
 
-#pragma vector = USCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
+#pragma vector = USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
 {
-  switch(__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG))
+  switch(__even_in_range(UCA1IV, USCI_UART_UCTXCPTIFG))
   {
     case USCI_NONE:
         {
@@ -48,8 +48,8 @@ __interrupt void USCI_A0_ISR(void)
         }
     case USCI_UART_UCRXIFG:
         {
-            while(!(UCA0IFG & UCTXIFG));
-            UCA0TXBUF = UCA0RXBUF;
+            while(!(UCA1IFG & UCTXIFG));
+            UCA1TXBUF = UCA1RXBUF;
             __no_operation();
             break;
         }
